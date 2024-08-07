3967616334ea42bb0a92bc0233a7bd3a2922c733
jonas <jj_jcs@hotmail.com>
2024-08-07 13:42:52 -0300

    feature: atualizado URL da API

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://nurse-marketplace-back.onrender.com';
 
 export const api = axios.create({
   baseURL: API_URL,
